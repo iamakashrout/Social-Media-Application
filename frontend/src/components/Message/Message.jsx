97f3e0cb4ba507d10b6c5d2222f0dffc9bbc4559
iamakashrout <iamakashrout@gmail.com>
2023-07-06 10:09:35 +0530

    profile pic added on messages

--- frontend/src/components/Message/Message.jsx
+++ frontend/src/components/Message/Message.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import "./Message.css";
 
-const Message = ({message, own}) => {
+const Message = ({ message, own }) => {
+  const token = useSelector((state) => state.token);
+  const [user, setUser] = useState(null);
+
+  const sender = message?.sender;
+
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:5000/users/${sender}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    setUser(data);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+
   return (
     <div className={own? "message own": "message"}>
       <div className="messageTop">
         <img
           className="messageImg"
-          src="https://images.pexels.com/photos/3686769/pexels-photo-3686769.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+          src={user?.picturePath}
           alt=""
         />
         <p className="messageText">{message?.text}</p>
