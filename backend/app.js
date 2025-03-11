b872f29c63641c9ddfb235125f6bfd50f015de65
Kanavpreet-Singh <145096601+Kanavpreet-Singh@users.noreply.github.com>
2025-03-12 00:23:04 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -136,7 +136,7 @@ mongoose
   });
 
 
-   const PORT = process.env.PORT || 5001;
+   const PORT = process.env.PORT || 5000;
    server.listen(PORT, () =>
      console.log(`Server successfully running on Port: ${PORT}`)
    );
